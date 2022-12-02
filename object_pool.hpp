e3fd49c1b538c16265f67af83f69ad63b236fe78
chenxi hou <chenxi.hou@grabtaxi.com>
2022-12-02 19:34:29 +0800

    improve format

--- object_pool.hpp
+++ object_pool.hpp
@@ -7,40 +7,40 @@
 template<class T>
 class Pool {
 public:
-	~Pool(){
-		for(auto iter : m_obj_list) {
-			delete iter;
-		}
-	}
+  ~Pool(){
+    for(auto iter : m_obj_list) {
+      delete iter;
+    }
+  }
 
-	T* Get();
-	void Put(T* obj);
+  T* Get();
+  void Put(T* obj);
 
 private:
-	std::mutex m_mutex;
-	std::list<T*> m_obj_list;
+  std::mutex m_mutex;
+  std::list<T*> m_obj_list;
 };
 
 template<class T>
 T* Pool<T>::Get() {
-    std::unique_lock<std::mutex> locker(m_mutex);
-	if (m_obj_list.empty()) {
-		auto obj = new T;
-		return obj;
-	}
+  std::unique_lock<std::mutex> locker(m_mutex);
+  if (m_obj_list.empty()) {
+    auto obj = new T;
+    return obj;
+  }
 
-	auto front = m_obj_list.front();
-	m_obj_list.erase(m_obj_list.begin());
+  auto front = m_obj_list.front();
+  m_obj_list.erase(m_obj_list.begin());
 
-	return front;
+  return front;
 }
 
 
 template<class T>
 void Pool<T>::Put(T* obj) {
-    std::unique_lock<std::mutex> locker(m_mutex);
+  std::unique_lock<std::mutex> locker(m_mutex);
 
-	m_obj_list.push_back(obj);
+  m_obj_list.push_back(obj);
 }
 
 #endif
